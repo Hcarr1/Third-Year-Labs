b167bb14f28707bef1c9dcef29146a09b9b09d27
Hcarr1 <h.carr@lancaster.ac.uk>
2025-10-20 20:55:32 +0100

    Initial commit for lab 1 server/client code

---
+++ SCC.331/Lab1/src/Shared/Args.java
@@ -0,0 +1,14 @@
+package Shared;
+
+// Server.Server.Args.java (place in server/ and client/)
+// This file will need a fix 
+public class Args {
+    public double a;
+    public double b;
+
+    public Args(double a, double b) {
+        this.a = a;
+        this.b = b;
+    }
+}
+
---
+++ SCC.331/Lab1/src/Shared/InvalidInputException.java
@@ -0,0 +1,6 @@
+package Shared;
+
+// Server.Server.InvalidInputException.java (place in server/ and client/)
+public class InvalidInputException extends Exception {
+    public InvalidInputException(String message) { super(message); }
+}
